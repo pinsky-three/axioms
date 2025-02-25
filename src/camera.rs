d3aab3ac7413a6b98f2a2be4ae79b1975b8967d2
Bregy Malpartida <bregy@minsky.cc>
2025-02-24 22:28:31 -0500

    refactor: simplify camera zoom system by removing unused window queries and adjusting scale limits

--- src/camera.rs
+++ src/camera.rs
@@ -1,28 +1,26 @@
 use bevy::input::gestures::PinchGesture;
 use bevy::input::mouse::MouseWheel;
 
-use bevy::window::PrimaryWindow;
-use bevy::{diagnostic::FrameTimeDiagnosticsPlugin, prelude::*};
+use bevy::prelude::*;
+// use bevy::window::PrimaryWindow;
 
 pub struct CommonPlugin;
 
 impl Plugin for CommonPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(FrameTimeDiagnosticsPlugin)
-            // .add_systems(Startup)
-            .add_systems(Update, (camera_zoom_system, camera_pan_system));
+        app.add_systems(Update, (camera_zoom_system, camera_pan_system));
     }
 }
 
 pub fn camera_zoom_system(
     mut camera: Query<Option<Mut<OrthographicProjection>>, With<Camera>>,
     mut evr_gesture_pinch: EventReader<PinchGesture>,
-    q_windows: Query<&Window, With<PrimaryWindow>>,
+    // q_windows: Query<&Window, With<PrimaryWindow>>,
 ) {
-    let win = q_windows.single();
+    // let win = q_windows.single();
 
-    let width = win.width();
-    let height = win.height();
+    // let width = win.width();
+    // let height = win.height();
 
     let amount = evr_gesture_pinch.read().map(|ev| ev.0).sum::<f32>();
 
@@ -35,14 +33,14 @@ pub fn camera_zoom_system(
             continue;
         };
 
-        let Some(current_cursor_pos) = win.cursor_position() else {
-            continue;
-        };
+        // let Some(current_cursor_pos) = win.cursor_position() else {
+        //     continue;
+        // };
 
         projection.scale -= amount;
-        projection.scale = projection.scale.clamp(0.001, 3.0);
-        projection.viewport_origin =
-            Vec2::new(current_cursor_pos.x / width, current_cursor_pos.y / height);
+        projection.scale = projection.scale.clamp(0.001, 10.0);
+        // projection.viewport_origin =
+        //     Vec2::new(current_cursor_pos.x / width, current_cursor_pos.y / height);
     }
 }
 
