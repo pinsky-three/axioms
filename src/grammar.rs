947d428cfa1394e60ffe2413f4fae373d1700720
Bregy Malpartida <bregy@minsky.cc>
2025-03-03 03:59:51 -0500

    feat: improve expression parsing with enhanced debug output and refine imaginary literal handling

--- src/grammar.rs
+++ src/grammar.rs
@@ -185,7 +185,11 @@ fn eval_expr(expr: &Expr, context: &mut ComplexMathContext) -> Complex<f64> {
 /// Recursively converts a Pest parse tree into our AST.
 fn parse_expr(pair: Pair<Rule>) -> Expr {
     // 2.2e*(-i*.2*z) + .4z^2
-    println!("parse_expr: {:?}", pair);
+    println!(
+        "parse_expr\t| rule: {:?}\t\t| span: {:?}",
+        pair.as_rule(),
+        pair.as_span().as_str()
+    );
     match pair.as_rule() {
         Rule::expression => {
             let inner = pair.into_inner().next().unwrap();
@@ -349,7 +353,7 @@ fn parse_expr(pair: Pair<Rule>) -> Expr {
             Expr::FuncDef(name, params, body)
         }
         Rule::imag_literal => {
-            let s: &str = pair.as_str().trim(); // e.g. "3i" or ".5i"
+            let s: &str = pair.as_str().trim().trim_end_matches("i"); // e.g. "3i" or ".5i"
             let val = s.parse::<f64>().unwrap();
 
             Expr::NumberImag(val) // or something representing 0 + val*i
